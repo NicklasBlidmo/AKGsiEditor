27d5d4067591b208399b137ecb80ecde93cd6dd9
Nicklas Blidmo <nicklas.blidmo@gmail.com>
2018-10-09 23:46:04 +0200

    todo: fix open new file

--- coordinate_editor.py
+++ coordinate_editor.py
@@ -15,7 +15,13 @@ It defines classes_and_methods
 @deffield    updated: Updated
 '''
 
+# GSI 16 word format
+#*110001+000RTCM-Ref 0454 81...6+0000006742974308 82...6+0000065558922759 83...6+0000000000436354 72....+0000000000000000 73....+0000000000000000 74....+0000000000000000 75....+0000000000000000 
+#*110002+0000000000020363 81...6+0000006774180503 82...6+0000065560868658 83...6+0000000000256145 72....+0000000000002022 73....+00000000000000KN 74....+0000000000000001 75....+0000000000000000 
+
+
 import sys
+import os.path
 
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
@@ -31,8 +37,7 @@ __updated__ = '2018-09-27'
 
 GSI_16_LEN = 24
 
-#*110001+000RTCM-Ref 0454 81...6+0000006742974308 82...6+0000065558922759 83...6+0000000000436354 72....+0000000000000000 73....+0000000000000000 74....+0000000000000000 75....+0000000000000000 
-#*110002+0000000000020363 81...6+0000006774180503 82...6+0000065560868658 83...6+0000000000256145 72....+0000000000002022 73....+00000000000000KN 74....+0000000000000001 75....+0000000000000000 
+
 
 class GsiWord():
     def __init__(self, raw_word_str, widgetText):
@@ -47,33 +52,53 @@ class MeasuredData(GsiWord):
         self.input_mode = raw_word_str[4]
         self.units = raw_word_str[5]
         self.data = raw_word_str[7:23]
-        super().__init__(raw_word_str, self.getText())
+        super().__init__(raw_word_str, self.getWidgetText())
     
-    def getText(self):
+    def getWidgetText(self):
         # Last four digits are decimals        
         formattedStr = self.data.lstrip("0")[:-4] + ',' + self.data.lstrip("0")[-4:]
         return formattedStr
-                                      
+    
+    def encode(self):
+        gsi_word_str = self.word_index + "..16" + self.sign + \
+        self.data.zfill(16)
+        return gsi_word_str
 
 class PointNumber(GsiWord):
     def __init__(self, raw_word_str):
         self.block_no = raw_word_str[2:6]
         self.point_id = raw_word_str[7:23]
-        super().__init__(raw_word_str, self.point_id.lstrip("0"))
+        super().__init__(raw_word_str, self.getWidgetText())
         
-    def getText(self):
-        return self.point_id
+    def getWidgetText(self):
+        return self.point_id.lstrip("0")
+    
+    def set_block_number(self, block_number):
+        self.block_no = block_number.zfill(4)
+        
+    def encode(self):
+        gsi_word_str = self.word_index + self.block_no + self.sign + \
+        self.point_id.zfill(16) 
+        return gsi_word_str   
+        
+        
         
 class Attribute(GsiWord):
     def __init__(self, raw_word_str):
         self.attribute_str = raw_word_str[7:23]
-        super().__init__(raw_word_str, self.attribute_str.lstrip("0")) 
+        super().__init__(raw_word_str, self.getWidgetText()) 
     
-    def getText(self):
-        return self.attribute_str               
+    def getWidgetText(self):
+        return self.attribute_str.lstrip("0")         
+    
+    def encode(self):
+        gsi_word_str = self.word_index + "...." + self.sign + self.attribute_str.zfill(16)
+        return gsi_word_str
         
 class GsiObject():
-    gsi_word_index = {'11': '0', '81': '1', '82': '2', '83': '3', '72': '4', '73': '5', '74': '6', '75': '7'} 
+    gsi_word_index = {'11': '0', '81': '1', '82': '2', '83': '3', '72': '4',\
+                       '73': '5', '74': '6', '75': '7'} 
+    
     def __init__(self, raw_string):
         self.raw_string = raw_string
         self.gsi_words = self.create_gsi_words()        
@@ -101,49 +126,24 @@ class GsiObject():
             return Attribute(word) 
         else:
             return None
-            
-
-def main(argv=None): # IGNORE:C0111
-    '''Command line options.'''
-
-    if argv is None:
-        argv = sys.argv
-    else:
-        sys.argv.extend(argv)
-    try:
-        # Setup argument parser
-        parser = ArgumentParser("AK Coordinate parser", formatter_class=RawDescriptionHelpFormatter)       
-        parser.add_argument("-v", "--verbose", action="count", default= 0, help="set verbosity level [default: %(default)s]")
-        parser.add_argument("source_file", help="source GSI file to parse")
-
-        # Process arguments
-        args = parser.parse_args()
-
-        verbose = args.verbose
- 
-        if verbose > 0:
-            print("Verbose mode on")
+    
+    def encode_to_gsi(self, block_number):
+                
+        # Set correct block number in first GSI Word
+        self.gsi_words[0].set_block_number(block_number)
+        gsi_word_list = ["*"]
+        # Then decode word for word
+        for word in self.gsi_words:
+            gsi_word_list.append(word.encode() + " ")
+        return ''.join(gsi_word_list)
         
-        coordinate_list = list()  
-        gsi_objects = list()
-        with open(args.source_file, "r") as gsiFile:
-            allLines = gsiFile.readlines()
-            for line in allLines:
-                coordinate_list.append(line.split())
-                gsi_objects.append(GsiObject(line))
-        
-        for cord in coordinate_list:
-            print(cord)
-              
-
-    except Exception as e:
-        sys.stderr.write("for help use --help  Exception: " + e )
-        return 2
 
 class CoEditorMainWin(QMainWindow):
     
     def __init__(self):
         super().__init__() 
+        self.currentFile = None
+        self.gsi_objects = list()
         self.initUI()
        
     def closeEvent(self, event):     
@@ -168,7 +168,7 @@ class CoEditorMainWin(QMainWindow):
         self.setCentralWidget(self.mainWidget)              
         
         self.setGeometry(300, 300, 1240, 600)
-        self.setWindowTitle('AK Coordinate editor')    
+        self.setWindowTitle('AK GSI Editor')    
         
         self.printFileAct = QAction('Print content...', self)
         self.printFileAct.triggered.connect(self.mainWidget._print_widget_content)
@@ -182,19 +182,41 @@ class CoEditorMainWin(QMainWindow):
         self.show()
     
     def _choose_gsi_file(self):
-        fileName, _ = QFileDialog.getOpenFileName(None, "Choose GSI file file", "", "GSI files (*.gsi)")                
-        if(len(fileName) != 0):
-            print(fileName)           
+        self.currentFile, _ = QFileDialog.getOpenFileName(None, "Choose GSI file file", "", "GSI files (*.gsi)")                
+        if(len(self.currentFile) != 0):
+            print(self.currentFile)           
         
-            coordinate_list = list()  
-            self.gsi_objects = list()
+            coordinate_list = list()             
             
-            with open(fileName, "r") as gsiFile:
+            with open(self.currentFile, "r") as gsiFile:
                 allLines = gsiFile.readlines()
                 for line in allLines:
                     coordinate_list.append(line.split())
                     self.gsi_objects.append(GsiObject(line))            
             self.mainWidget.fillTable(self.gsi_objects)
+    
+    def saveGsiFile(self):
+        # Find out path to current file
+        (current_dir, current_file) = os.path.split(self.currentFile)
+        (shortName, extension) = os.path.splitext(current_file)
+        
+        saveDir = os.path.dirname(current_dir) +"/Justerade"
+        saveFile = shortName +"_just"+ extension
+        
+        # Create Save directory (on level up)
+        if not os.path.isdir(saveDir):
+            os.makedirs(saveDir)
+        
+        fileName, _ = QFileDialog.getSaveFileName(None, "Choose data base file", os.path.join(saveDir, saveFile), "GSI files (*.gsi)")    
+        
+        gsi_string_list = list()
+        if fileName:
+            with open(fileName, "w") as targetFile:
+                for point_num, gsiObj in enumerate(self.gsi_objects):
+                    gsi_string_list.append(gsiObj.encode_to_gsi(str(point_num)))
+                
+                for line in gsi_string_list:
+                    targetFile.write(line + "\n")
             
     def print_gsi_objects(self):
         for gsiObj in self.gsi_objects:
@@ -226,28 +248,50 @@ class MainWidget(QWidget):
         
         mainWidgetVboxLayout = QVBoxLayout()
         
-        # Buttons
+        # Row Buttons
         commandBox  = QGroupBox()
-        commandBox.setTitle("Commands")
+        commandBox.setTitle("Radoperationer")
         commandLayout = QVBoxLayout()
         
-        self.addNewRowBtn = QPushButton("Add New Row")
-        self.delNewRowBtn = QPushButton("Delete Row")         
-        self.addNewRowBtn.setToolTip("Add new row above selected row")
-        self.delNewRowBtn.setToolTip("Add new row above selected row")
+        self.addNewRowBtn = QPushButton("Lägg till rad")
+        self.delNewRowBtn = QPushButton("Ta bort rad")         
+        self.addNewRowBtn.setToolTip("Lägg till en ny rad")
+        self.delNewRowBtn.setToolTip("Radera valda rader")
         self.addNewRowBtn.clicked.connect(self.add_row)
         self.delNewRowBtn.clicked.connect(self.delete_row)
 
         commandLayout.addWidget(self.addNewRowBtn) 
         commandLayout.addWidget(self.delNewRowBtn)
         commandBox.setLayout(commandLayout)        
-        commandHboxLayout = QHBoxLayout()        
-        commandHboxLayout.addWidget(commandBox)
-        commandHboxLayout.addStretch(1)
-        mainWidgetVboxLayout.addLayout(commandHboxLayout)  
         
-        # Table widget
+        # File buttons
+        fileBox  = QGroupBox()
+        fileBox.setTitle("Filoperationer")
+        fileBoxLayout = QVBoxLayout()
         
+        self.openFileBtn = QPushButton("Öppna fil")
+        self.saveFileBtn = QPushButton("Spara fil")         
+        self.openFileBtn.setToolTip("Öppna en ny GSI-fil")
+        self.saveFileBtn.setToolTip("Spara som en ny GSI-fil")
+        self.openFileBtn.clicked.connect(self.parent()._choose_gsi_file)
+        self.saveFileBtn.clicked.connect(self.parent().saveGsiFile)
+
+        fileBoxLayout.addWidget(self.openFileBtn) 
+        fileBoxLayout.addWidget(self.saveFileBtn)
+        fileBox.setLayout(fileBoxLayout)        
+       
+       
+        controlBoxHboxLayout = QHBoxLayout()        
+        controlBoxHboxLayout.addWidget(commandBox)
+        controlBoxHboxLayout.addSpacing(40)
+        controlBoxHboxLayout.addWidget(fileBox)
+        controlBoxHboxLayout.addStretch(1)
+        mainWidgetVboxLayout.addLayout(controlBoxHboxLayout)  
+        
+
+
+        
+        # Table widget        
         self.tableWidget = QTableWidget()
         self.tableWidget.setRowCount(30)
         self.tableWidget.setColumnCount(8)
@@ -273,19 +317,20 @@ class MainWidget(QWidget):
         
         
         self.setGeometry(0, 0, 1240, 600)
-#         pal = self.palette()
-#         pal.setColor(self.backgroundRole(), QColor(232, 252, 210))
-#         self.setPalette(pal)
+        pal = self.palette()
+        pal.setColor(self.backgroundRole(), QColor(255, 247, 204))
+        self.setPalette(pal)
 
         self.setAutoFillBackground(True)
         self.setWindowTitle('Buttons')     
         
     def delete_row(self):        
         rows = self.tableWidget.selectionModel().selectedRows()
-        for selRow in rows:
-            pointNoRawStr = self.tableWidget.item(selRow.row(), 0).raw_word_str
-            self.parent().remove_gsi_object(pointNoRawStr)
-            self.tableWidget.removeRow(selRow.row())
+        if len(rows) != 0:
+            for selRow in rows:
+                pointNoRawStr = self.tableWidget.item(selRow.row(), 0).raw_word_str
+                self.parent().remove_gsi_object(pointNoRawStr)
+                self.tableWidget.removeRow(selRow.row())
     
     def add_row(self):
         # Todo
